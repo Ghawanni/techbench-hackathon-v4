d449014033ff16cdc4fc0682359446f05292a1ee
Mohammed Ghawanni <m7md.ghawanni@gmail.com>
2019-01-31 22:19:03 +0300

    showcase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ShowcaseComponent } from './showcase/showcase.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ShowcaseComponent
   ],
   imports: [
     BrowserModule,
